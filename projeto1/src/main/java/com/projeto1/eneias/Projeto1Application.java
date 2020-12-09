e4d652dabb1c91996a661baa30d51a5ed0ad4298
Frente <Frente@Frente-PC>
2020-12-09 13:33:09 -0300

    Projeto1 spring-boot-propriedades-casadocodigo

--- projeto1/src/main/java/com/projeto1/eneias/Projeto1Application.java
+++ projeto1/src/main/java/com/projeto1/eneias/Projeto1Application.java
@@ -2,12 +2,19 @@ package com.projeto1.eneias;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Projeto1Application {
+public class Projeto1Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Projeto1Application.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Projeto1Application.class);
 	}
 
 }
